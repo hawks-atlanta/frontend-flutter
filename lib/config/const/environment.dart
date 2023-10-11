bca7f9e7f6713ac76fc5ed672a86a1ec43c9c7fe
Santiago Gaona <53282017+SantiagoGaonaC@users.noreply.github.com>
2023-10-11 18:41:45 -0500

    fix: direct connection to server

--- lib/config/const/environment.dart
+++ lib/config/const/environment.dart
@@ -2,7 +2,7 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 //patron adaptador, si quiero modificar algo de .env todo va a estar igual
 class Enviroment {
   static initEnviroment() async {
-    await dotenv.load(fileName: ".env");
+    await dotenv.load(fileName: "env");
   }
   static String apiURL = dotenv.env['API_URL'] ?? 'NoFoundENV';
 
